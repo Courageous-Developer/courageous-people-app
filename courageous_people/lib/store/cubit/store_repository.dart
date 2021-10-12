196dc3a33a2273b69df7f4139644ead1c2d3ad01
DDOLDDOL <doyt@naver.com>
2021-10-12 15:01:49 +0900

    refactor: Refactor store logic
    Refactor store logic

--- courageous_people/lib/store/cubit/store_repository.dart
+++ courageous_people/lib/store/cubit/store_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:courageous_people/common/constants.dart';
 import 'package:courageous_people/model/store_data.dart';
 import 'package:courageous_people/utils/interpreters.dart';
@@ -7,8 +9,29 @@ import '../../utils/http_client.dart';
 class StoreRepository {
   Future<List<Stores>> getStores() async {
     return storeInterpret((await httpRequestWithoutToken(
-        requestType: 'GET',
-        path: '/board/store'
-    )).body);
+        requestType: 'GET', path: '/board/store'))
+        .body);
+  }
+
+  Future<bool> addStore(
+      String storeName, String address, String post,
+      String? imageUrl, double latitude, double longitude, int registrant
+      ) async {
+    final response =  await httpRequestWithToken(
+      requestType: 'POST',
+      path: '/board/store',
+      body: {
+        "store_name": storeName,
+        "address": address,
+        "post": post,
+        "picture": imageUrl ?? '',  // todo: image url 추가
+        "latitude": latitude,
+        "longitude": longitude,
+        "user": registrant,
+      },
+    );
+
+    if(response.statusCode == 201)  return true;
+    return false;
   }
 }
