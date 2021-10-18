7dc948df4c656cfa3c54dfde3d70e76d303ec836
DDOLDDOL <doyt@naver.com>
2021-10-18 12:50:19 +0900

    refactor: Refactor menu data & store data
    Refactor menu data & store data

--- courageous_people/lib/model/menu_data.dart
+++ courageous_people/lib/model/menu_data.dart
@@ -1,3 +1,8 @@
-class Menu {
-  const Menu();
+class MenuData {
+  final String title;
+  final int price;
+  final int storeId;
+  final String imageUrl;
+
+  const MenuData(this.title, this.price, this.storeId, this.imageUrl);
 }
