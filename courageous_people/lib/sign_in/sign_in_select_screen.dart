7163e12dc78d4c05bd5bc122ab9181feed0c5536
DDOLDDOL <doyt@naver.com>
2021-10-24 17:52:56 +0900

    refactor: Refactor sign in screen
    Refactor sign in screen

--- courageous_people/lib/sign_in/sign_in_select_screen.dart
+++ courageous_people/lib/sign_in/sign_in_select_screen.dart
@@ -1,77 +1,5 @@
 import 'package:flutter/material.dart';
 import 'sign_in_screen.dart';
-//
-// class SignInSelectScreen extends StatelessWidget {
-//   const SignInSelectScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           Flexible(
-//             child: Container(
-//               padding: EdgeInsets.all(30),
-//               child: GestureDetector(
-//                 onTap: _onTap(context, true),
-//                 child: Container(
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.all(Radius.circular(30)),
-//                     border: Border.all(
-//                       color: Colors.green,
-//                       width: 10,
-//                     ),
-//                   ),
-//                   alignment: Alignment.center,
-//                   width: Size.infinite.width,
-//                   height: Size.infinite.height,
-//                   child: Text(
-//                     '손님 회원',
-//                     style: TextStyle(
-//                       color: Colors.black,
-//                       fontWeight: FontWeight.normal,
-//                       decoration: TextDecoration.none,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ),
-//           Flexible(
-//             child: Container(
-//               padding: EdgeInsets.all(30),
-//               child: GestureDetector(
-//                 onTap: _onTap(context, false),
-//                 child: Container(
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.all(Radius.circular(30)),
-//                     border: Border.all(
-//                       color: Colors.green,
-//                       width: 10,
-//                     ),
-//                   ),
-//                   alignment: Alignment.center,
-//                   width: Size.infinite.width,
-//                   height: Size.infinite.height,
-//                   child: Text(
-//                     '가게 회원',
-//                     style: TextStyle(
-//                       color: Colors.black,
-//                       fontWeight: FontWeight.normal,
-//                       decoration: TextDecoration.none,
-//                     ),
-//                   ),
-//                 ),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 class SignInSelectScreen extends StatelessWidget {
   const SignInSelectScreen({Key? key}) : super(key: key);
@@ -79,75 +7,68 @@ class SignInSelectScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.green,
-        elevation: 0,
-        title: Text('회원가입'),
+        backgroundColor: Colors.white,
+        elevation: 1,
+        centerTitle: true,
+        title: Text(
+          '회원가입',
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: Icon(Icons.arrow_back, color: Colors.black),
       ),
-      body: Center(
-        child:
-        Row(
+      body: SafeArea(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              child: GestureDetector(
-                onTap: () => _onTap(context, 2),//ToDo:회원가입 화면 전환
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          border: Border.all(color:Colors.lightGreenAccent,width: 5)
+            _selectBox(
+              right: 60,
+              bottom: 20,
+              padding: EdgeInsets.only(
+                top: 80, bottom: 40, left: 30, right: 30,
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => SignInScreen(managerFlag: 2),
+                  ),
+                );
+              },
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '사장님으로 회원가입',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30
+                        ),
                       ),
-                      height: 230,
-                      width: 190,
-                    ),
-                    Positioned.fill(
-                        bottom: 27,
-                        child: Align(alignment: Alignment.bottomCenter,
-                          child: Text('가게회원',
-                            style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold
-                            ),
-
-                          ),
-                        )
-                    ),
-                    Image.asset('assets/images/store.png',width: 190,),
-                  ],
-                ),
+                      Text('사장님으로 회원가입'),
+                    ],
+                  ),
+                ],
               ),
             ),
-            SizedBox(width: 10),
-            Container(
-              child: GestureDetector(
-                onTap: () => _onTap(context, 1),//ToDo:회원가입 화면 전환
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          border: Border.all(color:Colors.lightGreenAccent,width: 5)
-                      ),
-                      height: 230,
-                      width: 190,
-                    ),
-                    Positioned.fill(
-                        bottom: 27,
-                        child: Align(alignment: Alignment.bottomCenter,
-                          child: Text('일반회원',
-                            style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold
-                            ),
-                          ),
-                        )
-                    ),
-                    Image.asset('assets/images/container.png', width: 190),
-                  ],
-                ),
+            _selectBox(
+              right: 60,
+              bottom: 60,
+              padding: EdgeInsets.only(
+                top: 40, bottom: 80, left: 30, right: 30,
               ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => SignInScreen(managerFlag: 1),
+                  ),
+                );
+              },
             ),
           ],
         ),
@@ -155,11 +76,132 @@ class SignInSelectScreen extends StatelessWidget {
     );
   }
 
-  _onTap(BuildContext context, int managerFlag) {
-    try {
-      Navigator.push(context, MaterialPageRoute(
-        builder: (_) => SignInScreen(manageType: managerFlag),
-      ));
-    } catch ( Exception ) {};
+  Widget _selectBox({
+    required EdgeInsetsGeometry padding,
+    required double right,
+    required double bottom,
+    required void Function()? onTap,
+    Widget? child,
+  }) {
+    return Expanded(
+      child: Stack(
+        children: [
+          Padding(
+            padding: padding,
+            child: GestureDetector(
+              onTap: onTap,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.lightGreen.shade100,
+                  border: Border.all(width: 0.2, color: Colors.grey.shade500),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      offset: const Offset(2.4, 2.4),
+                    ),
+                  ],
+                ),
+                child: child,
+              ),
+            ),
+          ),
+          Positioned(
+            right: right,
+            bottom: bottom,
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.green.shade200,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade200,
+                    offset: const Offset(2, 2),
+                  ),
+                ],
+              ),
+              child: Icon(Icons.arrow_forward, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+// Center(
+// child: Row(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Container(
+// child: GestureDetector(
+// onTap: () => _onTap(context, 2), //ToDo:회원가입 화면 전환
+// child: Stack(
+// children: [
+// Container(
+// decoration: BoxDecoration(
+// borderRadius: BorderRadius.circular(15),
+// border: Border.all(color:Colors.lightGreenAccent,width: 5)
+// ),
+// height: 230,
+// width: 190,
+// ),
+// Positioned.fill(
+// bottom: 27,
+// child: Align(alignment: Alignment.bottomCenter,
+// child: Text('가게회원',
+// style: TextStyle(
+// fontSize: 22,
+// fontWeight: FontWeight.bold
+// ),
+//
+// ),
+// )
+// ),
+// Image.asset('assets/images/store.png',width: 190,),
+// ],
+// ),
+// ),
+// ),
+// SizedBox(width: 10),
+// Container(
+// child: GestureDetector(
+// onTap: () => _onTap(context, 1),//ToDo:회원가입 화면 전환
+// child: Stack(
+// children: [
+// Container(
+// decoration: BoxDecoration(
+// borderRadius: BorderRadius.circular(15),
+// border: Border.all(color:Colors.lightGreenAccent,width: 5),
+// ),
+// height: 230,
+// width: 190,
+// ),
+// Positioned.fill(
+// bottom: 27,
+// child: Align(alignment: Alignment.bottomCenter,
+// child: Text('일반회원',
+// style: TextStyle(
+// fontSize: 22,
+// fontWeight: FontWeight.bold
+// ),
+// ),
+// )
+// ),
+// Image.asset('assets/images/container.png', width: 190),
+// ],
+// ),
+// ),
+// ),
+// ],
+// ),
+// ),
